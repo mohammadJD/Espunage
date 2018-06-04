36f367dd4854555c1d6fecd4c9e621a19818e81d
HibaSaad <hiba150592@gmail.com>
2018-06-04 12:56:18 +0300

    api configeration

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -55,8 +55,30 @@ class ApiController extends Controller
         ];
     }
 
+
     public function actionTest()
     {
         return "hhhh";
+    }
+
+    public function actionGetProducts()
+    {
+        $ckfile1 = tempnam ("/tmp", "CURLCOOKIE");
+        $ch = curl_init('https://espunage.erpnext.com/api/resource/Item2');
+        //$ch = curl_init(erpUrl.'api/resource/Item2/?order_by=creation%20desc&limit_page_length=16&fields=["item_name","name","city","standard_rate","description","image","website_image"]');
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $ckfile1);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $ckfile1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+        curl_setopt($ch, CURLOPT_TIMEOUT, (10));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        $response = curl_exec($ch);
+        $header = curl_getinfo($ch);
+        $body = json_decode($response);
+
+        return $response;
     }
 }
